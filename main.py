09b19a2fb86e01424201d088e412b78b6be7e2fc
Neulovim <neulovimbiy@gmail.com>
2020-10-11 01:37:27 +0300

    don't work

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import numpy as np
+
 from data import TrainingData
+from network import Network
 
 inputData = [9, 8, 7]
 # inputData = [5, 4, 3]
@@ -8,3 +11,14 @@ training.create_training_list()
 training.create_bool_function_results()
 training.print_result()
 
+# набор входных данных
+X = np.array([[0, 0, 1],
+              [0, 1, 1],
+              [1, 0, 1],
+              [1, 1, 1]])
+
+# выходные данные
+y = np.array([[0, 0, 1, 1]]).T
+
+network = Network(X, y)
+network.get_network_results()
--- network.py
+++ network.py
@@ -1,43 +1,62 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-# Сигмоида
-def nonlin(x, deriv=False):
-    if deriv == True:
-        return x * (1 - x)
-    return 1 / (1 + np.exp(-x))
+def lineplot(x_data, y_data, x_label="", y_label="", title=""):
+    # Create the plot object
+    _, ax = plt.subplots()
+
+    # Plot the best fit line, set the linewidth (lw), color and
+    # transparency (alpha) of the line
+    ax.plot(x_data, y_data, lw=2, color='#539caf', alpha=1)
+
+    # Label the axes and provide a title
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    plt.show()
 
 
-# набор входных данных
-X = np.array([[0, 0, 1],
-              [0, 1, 1],
-              [1, 0, 1],
-              [1, 1, 1]])
+class Network(object):
+    def __init__(self, input_data, output_data):
+        self._input_data = input_data
+        self._output_data = output_data
+        # сделаем случайные числа более определёнными
+        np.random.seed(1)
+        # инициализируем веса случайным образом со средним 0
+        self._syn0 = 0
 
-# выходные данные
-y = np.array([[0, 0, 1, 1]]).T
+    def get_network_results(self):
+        self._syn0 = 2 * np.random.random((3, 1)) - 1
+        for iteration in range(10000):
+            # прямое распространение
+            l0 = self._input_data
+            l1 = self._get_sigmoid_prime(np.dot(l0, self._syn0))
+            # if iteration == 9999:
+            #     print(l1_delta)
+            #     print(np.dot(l0, syn0))
+            #     lineplot(np.dot(l0, syn0), l1)
 
-# сделаем случайные числа более определёнными
-np.random.seed(1)
+            # насколько мы ошиблись?
+            l1_error = self._output_data - l1
 
-# инициализируем веса случайным образом со средним 0
-syn0 = 2 * np.random.random((3, 1)) - 1
+            # перемножим это с наклоном сигмоиды
+            # на основе значений в l1
+            l1_delta = l1_error * self._get_sigmoid(l1)  # !!!
 
-for iteration in range(10000):
-    # прямое распространение
-    l0 = X
-    l1 = nonlin(np.dot(l0, syn0))
+            # обновим веса
+            self._syn0 += np.dot(l0.T, l1_delta)  # !!!
 
-    # насколько мы ошиблись?
-    l1_error = y - l1
+        print("Выходные данные после тренировки:")
+        print(l1)
 
-    # перемножим это с наклоном сигмоиды
-    # на основе значений в l1
-    l1_delta = l1_error * nonlin(l1, True)  # !!!
+    @staticmethod
+    def _get_sigmoid(x):
+        """Сигмоида."""
+        return 1.0 / (1.0 + np.exp(-x))
 
-    # обновим веса
-    syn0 += np.dot(l0.T, l1_delta)  # !!!
-
-print("Выходные данные после тренировки:")
-print(l1)
+    def _get_sigmoid_prime(self, x):
+        """Производная сигмоиды."""
+        return self._get_sigmoid(x) * (1 - self._get_sigmoid(x))
+        # return x * (1 - x)
