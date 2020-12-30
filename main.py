575049ece7501a2dfbc6b8695809ca6abd2e8c2f
Neulovim <neulovimbiy@gmail.com>
2020-12-30 17:15:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,39 +9,27 @@ def sigmoid_prime(x):
     return sigmoid(x) * (1.0 - sigmoid(x))
 
 
-def tanh(x):
-    return np.tanh(x)
-
-
-def tanh_prime(x):
-    return 1.0 - x ** 2
-
-
 class NeuralNetwork:
 
-    def __init__(self, layers, activation='tanh'):
-        if activation == 'sigmoid':
-            self.activation = sigmoid
-            self.activation_prime = sigmoid_prime
-        elif activation == 'tanh':
-            self.activation = tanh
-            self.activation_prime = tanh_prime
+    def __init__(self, layers):
+        self.activation = sigmoid
+        self.activation_prime = sigmoid_prime
 
-        # Set weights
+        # Установить веса
         self.weights = []
-        # layers = [2,2,1]
-        # range of weight values (-1,1)
-        # input and hidden layers - random((2+1, 2+1)) : 3 x 3
+        # слои = [3,3,2]
+        # диапазон значений веса (-1,1)
+        # входные и скрытые слои - random((2+1, 2+1)) : 3 x 3
         for i in range(1, len(layers) - 1):
             r = 2 * np.random.random((layers[i - 1] + 1, layers[i] + 1)) - 1
             self.weights.append(r)
-        # output layer - random((2+1, 1)) : 3 x 1
+        # выходной слой - random((2+1, 1)) : 3 x 1
         r = 2 * np.random.random((layers[i] + 1, layers[i + 1])) - 1
         self.weights.append(r)
 
     def fit(self, X, y, learning_rate=0.2, epochs=100000):
-        # Add column of ones to X
-        # This is to add the bias unit to the input layer
+        # Добавить столбец ones к X
+        # Это необходимо для добавления блока смещения к входному слою.
         ones = np.atleast_2d(np.ones(X.shape[0]))
         X = np.concatenate((ones.T, X), axis=1)
 
@@ -53,7 +41,7 @@ class NeuralNetwork:
                 dot_value = np.dot(a[l], self.weights[l])
                 activation = self.activation(dot_value)
                 a.append(activation)
-            # output layer
+            # выходной слой
             error = y[i] - a[-1]
             deltas = [error * self.activation_prime(a[-1])]
 
@@ -62,19 +50,19 @@ class NeuralNetwork:
                 print('error:', error)
                 print('deltas:', deltas)
 
-            # we need to begin at the second to last layer
-            # (a layer before the output layer)
+            # нам нужно начать с предпоследнего слоя
+            # (слой перед выходным слоем)
             for l in range(len(a) - 2, 0, -1):
                 deltas.append(deltas[-1].dot(self.weights[l].T) * self.activation_prime(a[l]))
 
-            # reverse
+            # обратный
             # [level3(output)->level2(hidden)]  => [level2(hidden)->level3(output)]
             deltas.reverse()
 
             # backpropagation
-            # 1. Multiply its output delta and input activation
-            #    to get the gradient of the weight.
-            # 2. Subtract a ratio (percentage) of the gradient from the weight.
+            # 1. Умножьте его выходную дельту и входную активацию,
+            #     чтобы получить градиент веса.
+            # 2. Вычтите отношение (процент) градиента из веса.
             for i in range(len(self.weights)):
                 layer = np.atleast_2d(a[i])
                 delta = np.atleast_2d(deltas[i])
@@ -120,7 +108,7 @@ if __name__ == '__main__':
                   [7, 8, 6]])
     y = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1])
 
-    nn.fit(X, y, epochs=190001) # 20000
+    nn.fit(X, y, epochs=600001) # 20000
     x_test = np.array([[7, 8, 8],
                        [7, 7, 7],
                        [7, 7, 6],
@@ -131,8 +119,6 @@ if __name__ == '__main__':
     y_test = np.array([1, 1, 1, 1, 1, 1, 1])
     for elem in range(len(x_test)):
         print(x_test[elem], nn.predict(x_test[elem]), y_test[elem])
-    # for elem in range(len(X)):
-    #     print(X[elem], nn.predict(X[elem]), y[elem])
 
     x = np.array(
         [[0, 0, 0], [0, 0, 1], [0, 0, 2], [0, 0, 3], [0, 0, 4], [0, 0, 5], [0, 0, 6], [0, 0, 7], [0, 0, 8], [0, 0, 9],
